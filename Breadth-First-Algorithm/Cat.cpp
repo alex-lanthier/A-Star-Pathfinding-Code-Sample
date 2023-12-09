f9b8062e59cf5e414ddabf85734a3648425040ba
Alex-Lanthier <alanthier@optonline.net>
2023-12-08 20:14:45 -0500

    removing description from the beginning of script because of the added README file

--- Breadth-First-Algorithm/Cat.cpp
+++ Breadth-First-Algorithm/Cat.cpp
@@ -1,15 +1,3 @@
-//I made this as a part of an AI Programming class. 
-//This algorithm was framed as a game where a certain agent (called a 'cat') tries to find its
-//way out of a grid by moving itself towards the outside border. At the start there are
-//a few randomly generated blockers that take up a grid space, and more blockers are
-//spawned at the point the cat is trying to get to in an attempt to fully wall it in.
-//Because of this, some variables and classes are reliant or inherited from the given
-//cMake framework code. I left the framework out because I'm just trying to display the specific algorithm.
-
-//To see this in operation, see https://github.com/alex-lanthier/AI_Lab_02 and select the catchthecat startup item.
-//When the win or loss condition is met, the program aborts. This sample is cleaned up from the original version,
-//but the functionality is the same
-
 #include "Cat.h"
 #include "World.h"
 
